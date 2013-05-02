b9d5c449863e5b1ac3df5eb612308732a2f48535
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-05-02 21:33:50 +0200

    [FEATURE] Add more default configuration

--- Resources/Private/Subpackages/Bootstrap/Configuration/PackageConfiguration.php
+++ Resources/Private/Subpackages/Bootstrap/Configuration/PackageConfiguration.php
@@ -37,8 +37,11 @@ return array(
 	'FE/pageNotFound_handling' => t3lib_div::getIndpEnv('TYPO3_SITE_URL').'index.php?id=16',
 
 	// Added by the Bootstrap package
+	'EXT/extConf/scheduler' => 'a:4:{s:11:"maxLifetime";s:4:"1440";s:11:"enableBELog";s:1:"1";s:15:"showSampleTasks";s:1:"1";s:11:"useAtdaemon";s:1:"0";}',
+	'EXT/extConf/seo_basics' => 'a:2:{s:10:"xmlSitemap";s:1:"1";s:16:"sourceFormatting";s:1:"1";}',
 	'EXT/extConf/flux' => 'a:2:{s:9:"debugMode";s:1:"0";s:7:"compact";s:1:"0";}',
 	'EXT/extConf/realurl' => 'a:5:{s:10:"configFile";s:21:"typo3conf/RealUrl.php";s:14:"enableAutoConf";s:1:"1";s:14:"autoConfFormat";s:1:"0";s:12:"enableDevLog";s:1:"0";s:19:"enableChashUrlDebug";s:1:"0";}',
 	'BE/RTE_imageStorageDir' => '1:variants',
+	'EXT/extConf/media' => 'a:24:{s:13:"updateMessage";s:1:"0";s:7:"storage";s:1:"1";s:13:"media_folders";s:2:"99";s:16:"category_folders";s:0:"";s:18:"default_categories";s:1:"3";s:24:"mount_point_for_variants";s:1:"4";s:27:"mount_point_for_file_type_1";s:1:"0";s:27:"mount_point_for_file_type_2";s:1:"2";s:27:"mount_point_for_file_type_3";s:1:"0";s:27:"mount_point_for_file_type_4";s:1:"0";s:27:"mount_point_for_file_type_5";s:1:"3";s:15:"image_thumbnail";s:7:"100x100";s:10:"image_mini";s:7:"120x120";s:11:"image_small";s:7:"320x320";s:12:"image_medium";s:7:"760x760";s:11:"image_large";s:9:"1200x1200";s:14:"image_original";s:9:"1920x1920";s:10:"permission";s:1:"0";s:29:"extension_allowed_file_type_1";s:9:"txt, html";s:29:"extension_allowed_file_type_2";s:40:"jpg, jpeg, bmp, png, tiff, tif, gif, eps";s:29:"extension_allowed_file_type_3";s:23:"mp3, mp4, m4a, wma, f4a";s:29:"extension_allowed_file_type_4";s:60:"mov, avi, mpeg, mpg, mp4, m4v, flv, f4v, webm, wmv, ogv, 3gp";s:29:"extension_allowed_file_type_5";s:79:"doc, docx, dotx, ppt, pptx, pps, ppsx, odt, xls, xlsx, xltx, pdf, zip, rtf, xlt";s:17:"mime_type_allowed";s:58:"image/*,audio/*,video/*,application/*,text/plain,text/html";}',
 );
 ?>
